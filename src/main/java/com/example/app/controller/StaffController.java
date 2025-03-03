5c78283ef8e95d6644a322883d77169dd3c97e8d
zd3N08 <dounaritai@icloud.com>
2025-03-03 15:04:23 +0900

    資材個別表示メソッドを追加

--- src/main/java/com/example/app/controller/StaffController.java
+++ src/main/java/com/example/app/controller/StaffController.java
@@ -3,26 +3,45 @@ package com.example.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.app.domain.Item;
 import com.example.app.service.ItemService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/")
+
 
 // 一般社員用ページと連携するコントローラ
 public class StaffController {
 	private final ItemService itemService;
 
-	@GetMapping // "/" への GET リクエストを処理
-	public String index(Model model) {
-		// 取得したリストをビューに渡す
-		model.addAttribute("itemList", itemService.getAll());
-		// index.html へフォワード
-		return "index";
-	}
+	// 資材リスト
+		@GetMapping("/")
+		public String index(Model model) {
+			model.addAttribute("itemList", itemService.getAll());
+			return "index";
+		}
 
-}
+
+		// 資材個別表示
+		@GetMapping("/show")
+		public String show(
+				@RequestParam(name = "id", required = false) Integer id,
+				Model model) {
+			Item item = null;
+			if(id != null) {
+				item = itemService.getOneById(id);
+			}
+
+			if(item == null) {
+				return "redirect:/";
+			}
+
+			model.addAttribute("item", item);
+			return "show";
+		}
+
+	}
