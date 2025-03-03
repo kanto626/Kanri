201af509c8154382f00b4682904217e050db581a
zd3N08 <dounaritai@icloud.com>
2025-03-03 15:12:26 +0900

    資材ID を元に1 件分の備品情報を返すメソッドを追加

--- src/main/java/com/example/app/service/ItemServiceImpl.java
+++ src/main/java/com/example/app/service/ItemServiceImpl.java
@@ -3,19 +3,43 @@ package com.example.app.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.app.domain.Item;
+import com.example.app.domain.Placement;
 import com.example.app.mapper.ItemMapper;
+import com.example.app.mapper.PlacementMapper;
 
 import lombok.RequiredArgsConstructor;
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
 	private final ItemMapper itemMapper;
+	private final PlacementMapper placementMapper;
 
 	@Override
 	public List<Item> getAll() {
 		return itemMapper.selectAll();
+	}
+
+	@Override
+	public Item getOneById(int id) {
+		Item item = itemMapper.selectById(id);
+
+		// 配置情報
+		List<Placement> list = placementMapper.selectByItemId(id);
+
+		// 資材の総数
+		int amount = 0;
+		for(Placement p : list) {
+			amount += p.getAmount();
+		}
+
+		// itemに配置情報、資材総数をまとめる
+		item.setPlacementList(placementMapper.selectByItemId(id));
+		item.setAmount(amount);
+		return item;
 	}
 }
