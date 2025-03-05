d153a37bce23eb66d25ab4189878578cc3a23a54
zd3N08 <dounaritai@icloud.com>
2025-03-05 13:21:55 +0900

    ページに関するデータを取得するメソッドを追加

--- src/main/java/com/example/app/service/ItemService.java
+++ src/main/java/com/example/app/service/ItemService.java
@@ -9,7 +9,25 @@ public interface ItemService {
 	List<Item> getAll();
 
 	Item getOneById(int id); // 資材IDを元に1件分の資材情報を返す
-	
-	// 資材IDを元に、ID、品名、総数、購入日、備考のリストを返す
+
+	// 場所IDを元に、ID、品名、総数、購入日、備考のリストを返す
 	List<Item> getByRoomId(String roomId);
+
+	// 引数としてページ番号を渡すと、そのページに該当する備品リストを返す
+	List<Item> getByPage(int page);
+
+	// 引数として場所ID とページ番号を渡すと、その場所とページに該当する備品リストを返す
+	List<Item> getByRoomIdAndPage(String roomId, int page);
+
+	// １ページあたりのデータ取得件数を設定する
+	void setNumPerPage(int numPerPage);
+
+	// １ページあたりのデータ取得件数を返す
+	int getNumPerPage();
+
+	// 全備品リストを表示するにあたって必要なページ数を返す
+	int getTotalPages();
+
+	// 場所IDで絞り込んだ備品リストを表示するにあたって必要なページ数を返す
+	int getTotlaPagesByRoomId(String roomId);
 }
