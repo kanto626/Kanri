56244c7c5f4cc03be8d8bd1cf009a9834121b827
zd3N08 <dounaritai@icloud.com>
2025-03-05 14:06:12 +0900

    selectLimitedとselectLimitedByRoomIdを追加

--- src/main/java/com/example/app/mapper/ItemMapper.java
+++ src/main/java/com/example/app/mapper/ItemMapper.java
@@ -3,6 +3,7 @@ package com.example.app.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.app.domain.Item;
 
@@ -11,7 +12,12 @@ public interface ItemMapper {
 	List<Item> selectAll();
 
 	Item selectById(int id); // 資材IDを元に1件分の資材情報を返す
-	
+
 	// 場所IDを元に、ID、品名、総数、購入日、備考のリストを返す
 	List<Item> selectByRoomId(String roomId);
+
+	List<Item> selectLimited(@Param("offset") int offset, @Param("num") int num);
+
+	List<Item> selectLimitedByRoomId(@Param("roomId") String roomId, @Param("offset") int offset,
+			@Param("num") int numPerPage);
 }
