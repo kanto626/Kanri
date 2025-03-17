092bf91e08f2882c59c6f2bc70edcb6af3c177fd
zd3N08 <dounaritai@icloud.com>
2025-03-17 13:52:59 +0900

    BCryptの設定と実装

---
+++ src/main/java/com/example/app/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.example.app.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.app.filter.AuthFilter;
+
+@Configuration
+// AuthFilter を有効化するための設定ファイル
+public class FilterConfig implements WebMvcConfigurer{
+
+	// 認証用フィルタの有効化
+	@Bean
+	FilterRegistrationBean<AuthFilter> authFilter() {
+	var bean =
+	new FilterRegistrationBean<AuthFilter>(new AuthFilter());
+	bean.addUrlPatterns("/admins/*");
+	return bean;
+	}
+}
--- src/main/java/com/example/app/service/AdminServiceImpl.java
+++ src/main/java/com/example/app/service/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.app.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,22 +13,23 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class AdminServiceImpl implements AdminService{
-	
+public class AdminServiceImpl implements AdminService {
+
 	private final AdminMapper adminMapper;
 
 	@Override
 	public boolean login(String loginId, String loginPass, HttpSession session) {
-		
-		// 
+
 		Admin admin = adminMapper.selectByLoginId(loginId);
-		
 
 		// ログインIDに該当する管理者がいない
-		if(admin == null) {
+		if (admin == null) {
 			return false;
 		}
-
+		// パスワードが異なる
+		if (!BCrypt.checkpw(loginPass, admin.getLoginPass())) {
+			return false;
+		}
 		// ログインID・パスワードが正しい
 		// ⇒ 管理者氏名をセッションに格納
 		session.setAttribute("name", admin.getName());
