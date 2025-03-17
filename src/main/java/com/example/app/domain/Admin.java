51c859ccad3371aed29e9e70f48ded6194333588
zd3N08 <dounaritai@icloud.com>
2025-03-17 13:52:16 +0900

    Adminクラスのフィールドに@NotBlankを追加

--- src/main/java/com/example/app/domain/Admin.java
+++ src/main/java/com/example/app/domain/Admin.java
@@ -1,12 +1,14 @@
 package com.example.app.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 // 管理者情報を保持するためのクラス
 public class Admin {
-
+	@NotBlank
 	private String loginId;
+	@NotBlank
 	private String loginPass;
 	private String name;
 
