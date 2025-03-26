9ea789f3acdb104fd515832e13a32f1fcc719eb6
zd3N08 <dounaritai@icloud.com>
2025-03-26 13:58:08 +0900

    個数と購入日のバリデーションを追加

--- src/main/java/com/example/app/domain/Item.java
+++ src/main/java/com/example/app/domain/Item.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -14,21 +18,27 @@ import lombok.Data;
 public class Item {
 
 	private Integer id;
-	
+
 	@NotBlank
 	@Size(max = 30)
 	private String name;
 	
+	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate purchasedAt;
-	
+
 	@Size(max = 255)
 	private String note;
 
 	// テーブル連携時
+	@NotNull
+	@Min(value = 0)
+	@Max(value = 9999)
 	private Integer amount;
-	private List<Placement> placementList; // 配置情報を保持
 	
+	@Valid
+	private List<Placement> placementList; // 配置情報を保持
+
 	// カテゴリを追加
 	@NotBlank
 	@Size(max = 30)
