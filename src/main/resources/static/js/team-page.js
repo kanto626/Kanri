5daf6fd1ebdfa5475bd885e2f1a150e17fb23c58
kai <dounaritai@icloud.com>
2025-06-23 23:36:08 +0900

    team用トップページのjsファイルを作成

---
+++ src/main/resources/static/js/team-page.js
@@ -0,0 +1,34 @@
+// select要素変更時に送信処理
+const selects = document.querySelectorAll('select');
+selects.forEach((select) => {
+  select.addEventListener('change', function () {
+    this.form.submit(); //  location.reload();では選択された値をサーバーに渡せない
+  });
+});
+
+const selectedRoomId = /*[[${roomId}]]*/ 'ALL';
+
+// クリックで選択(ハイライト)＆ roomId + category を維持して遷移
+
+const areas = document.querySelectorAll('.area');
+const currentCategory = document.querySelector('select[name="category"]').value;
+
+if (selectedRoomId !== 'ALL') {
+  const selectedArea = document.querySelector(
+    `.area[data-room="${selectedRoomId}"]`
+  );
+  if (selectedArea) {
+    selectedArea.classList.add('selected');
+  }
+}
+
+areas.forEach((area) => {
+  area.addEventListener('click', () => {
+    areas.forEach((a) => a.classList.remove('selected'));
+    area.classList.add('selected');
+
+    const roomId = area.dataset.room;
+    // roomId + 現在のcategoryをURLに渡す
+    window.location.href = `/team?roomId=${roomId}&category=${currentCategory}`;
+  });
+});
